0330aa94f7be1d1ff9874564095dbb63d7b1e66c
hrisdonchev <125268818+hrisdonchev@users.noreply.github.com>
2023-03-02 11:07:26 +0200

    Update update-install.sh

--- update-install.sh
+++ update-install.sh
@@ -7,10 +7,10 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Install curl
-sudo apt install curl
+sudo apt install curl -y
 
 # Install mtr and traceroute
-sudo apt install mtr traceroute
+sudo apt install mtr traceroute -y
 
 # Install Speedtest CLI
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
